3b6000a1209c84c56ac3387405aef10caf4b84b2
Blanca Hernandez <blamca1029@gmail.com>
2019-04-02 20:34:54 -0700

    fixed project images and other css

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 'use strict';
-function createMobileNav() {
-$('.mobile-toggle').click(function(){
-  $('.navigation').slideToggle();
-});
-}
 
+//Define functions
+  //When the user clicks on mobile navigation
+  function createMobileNav() {
+  $('.mobile-toggle').click(function(){
+    $('.navigation').slideToggle();
+  });
+  }
 
-function runAll() {
+//when the page loads...
+$(document).ready(function () {
   createMobileNav();
-}
+  $('#projects').hide();
+});
 
-$(function() {
-  runAll();
+//When the user clicks on project
+$('#view-projects').on('click', function () {
+  $('main').hide();
+  $('#projects').show();
+});
+
+//When the user clicks on 'Blanca Hernandez' it takes them back to the landing page
+$('#blanca').on('click', function () {
+  $('main').hide();
+  $('#landing-page').show();
 });
